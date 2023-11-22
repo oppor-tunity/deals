593efa42f8cd44db760c1430c5bf57d951727599
oppor-tunity <philip.oppor@gmail.com>
2023-11-22 21:26:46 +0100

    Replcae 'Switch' with 'Routes' in react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 // src/App.js
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import HomePageContainer from './containers/HomePageContainer';
@@ -12,12 +12,12 @@ function App() {
     <Router>
       <div className="App">
         <Header />
-        <Switch>
+        <Routes>
           <Route path="/" exact component={HomePageContainer} />
           <Route path="/category/:category" component={CategoryPageContainer} />
           {/* Add more routes as needed */}
           <Route path="/user/:userId" component={UserDashboardContainer} />
-        </Switch>
+        </Routes>
         <Footer />
       </div>
     </Router>

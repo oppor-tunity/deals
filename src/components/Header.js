5084e115924eb3497b505f57629da71cace36170
oppor-tunity <philip.oppor@gmail.com>
2023-11-23 10:46:14 +0100

    Remove DealSubmission from Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,5 @@
 // Header.js
 import React, { useState } from 'react';
-import DealSubmission from '../components/DealSubmission';
 import { Link } from 'react-router-dom';
 
 const Header = () => {
